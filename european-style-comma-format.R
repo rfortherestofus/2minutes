52cd15fa8c5f813c9c384c579f369b11158654a5
David Keyes <david@rfortherestofus.com>
2024-02-13 14:15:26 -0800

    Updates

---
+++ european-style-comma-format.R
@@ -0,0 +1,13 @@
+library(scales)
+
+comma(
+  x = 123.45,
+  accuracy = 0.01
+)
+
+comma(
+  x = 123.45,
+  accuracy = 0.01,
+  big.mark = ".",
+  decimal.mark = ","
+)
---
+++ rounding.R
@@ -0,0 +1,7 @@
+round(x = 11.5, digits = 0)
+round(x = 12.5, digits = 0)
+
+library(janitor)
+
+round_half_up(x = 11.5, digits = 0)
+round_half_up(x = 12.5, digits = 0)
